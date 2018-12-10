7793e73c8f8d9a704da480bfd98d7edb3e763d32
LinaNaef <paulinanaef@hotmail.com>
2018-12-10 13:29:14 +0100

    Added way to analyze DNA sequences in workflow_reference.py

--- TRAL_Analytics/workflow_reference.py
+++ TRAL_Analytics/workflow_reference.py
@@ -8,19 +8,24 @@ import os
 import get_sequences
 import find_TRs_in_genes
 
-working_directory = "/home/lina/SynologyDrive/TRAL_Masterthesis/Uniprot_data/pickles"
-sequences_path = "/home/lina/SynologyDrive/TRAL_Masterthesis/Uniprot_data"
+# ## AA reference
+# working_directory = "/home/lina/Desktop/TRAL_Masterthesis/references/Uniprot_data/pickles"
+# sequences_path = "/home/lina/Desktop/TRAL_Masterthesis/references/Uniprot_data"
+
+## DNA reference
+working_directory = "/home/lina/Desktop/TRAL_Masterthesis/references/NCBI/pickles"
+sequences_path = "/home/lina/Desktop/TRAL_Masterthesis/references/NCBI"
 
 ##########################################################################
 ### Defining paths and variables
 ##########################################################################
 
 genes = ["TGFBR2"]
-data_path = "/home/lina/SynologyDrive/TRAL_Masterthesis/Uniprot_data"
-sequences = get_sequences.sequences_per_gene(genes, sequences_path, show_time=False, as_pickle=False, patient=False)
-detectors = ["XSTREAM","HHrepID","T-REKS","TRUST"] # AA compatible detectors
 gene = "TGFBR2"
 
+sequences = get_sequences.sequences_per_gene(genes, sequences_path, show_time=False, as_pickle=False, patient=False)
+detectors_AA = ["XSTREAM","HHrepID","T-REKS","TRUST"] # AA compatible detectors
+detectors_DNA = ["T-REKS", "TRF", "XSTREAM"] # DNA compatible detectors (without TRED and Phobos)
 
 
 def get_TRs_per_detector(detectors, sequences, gene):
@@ -30,18 +35,19 @@ def get_TRs_per_detector(detectors, sequences, gene):
         TRs_per_detector.update({detector:TRs})
     return TRs_per_detector
 
-TRs_per_detector = get_TRs_per_detector(detectors, sequences, gene)
+TRs_per_detector = get_TRs_per_detector(detectors_DNA, sequences, gene)
+
+# for detector, TRs in TRs_per_detector.items():
+#     print("\n",detector,"\n")
+#     find_TRs_in_genes.loop_list_TR(TRs)
+
+
+## does not work till now: will try to perform statistical significance test
 
 for detector, TRs in TRs_per_detector.items():
     print("\n",detector,"\n")
-    find_TRs_in_genes.loop_list_TR(TRs)
+
+    for TR in TRs.repeats:
+        TR.calculate_pvalues()
 
 
-# TRs_XSTREAM = find_TRs_in_genes.get_TRs("XSTREAM", sequences["TGFBR2"])
-# TRs_HHrepID = find_TRs_in_genes.get_TRs("HHrepID", sequences["TGFBR2"])
-# TRs_TREKS = find_TRs_in_genes.get_TRs("T-REKS", sequences["TGFBR2"])
-# TRs_TRUST = find_TRs_in_genes.get_TRs("TRUST", sequences["TGFBR2"])
-# find_TRs_in_genes.loop_list_TR(TRs_XSTREAM)
-# find_TRs_in_genes.loop_list_TR(TRs_HHrepID)
-# find_TRs_in_genes.loop_list_TR(TRs_TREKS)
-# find_TRs_in_genes.loop_list_TR(TRs_TRUST)
