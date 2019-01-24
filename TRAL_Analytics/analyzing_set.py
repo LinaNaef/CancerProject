7c9e4809b2fe9fe1e88255f0732b4cffb74ad108
LinaNaef <paulinanaef@hotmail.com>
2019-01-24 12:36:40 +0100

    analyze_set add lenght distribution of TRs

--- TRAL_Analytics/analyzing_set.py
+++ TRAL_Analytics/analyzing_set.py
@@ -59,6 +59,10 @@ result_dir = os.path.join(output_path, set_name)
 
 all_AA = ""
 
+count_dist = {1:{},2:{},3:{}}
+max_unit_count = 20 # expected max count for visualization of unit length, unit count distribution
+
+
 ##########################################################################
 ######### Getting sequences
 
@@ -72,8 +76,6 @@ number_TR_proteins = 0
 number_TRs = 0
 
 max_length = 41
-l_n = np.zeros((4,max_length))
-print(l_n)
 
 for pyfaidx in proteins:
     number_proteins += 1
@@ -93,65 +95,88 @@ for pyfaidx in proteins:
             # print(denovo_list_remastered.repeats[TR])
             l = denovo_list_remastered.repeats[TR].l_effective
             n = denovo_list_remastered.repeats[TR].n
+            if n in count_dist[l]:
+                count_dist[l][n] += 1
+            else:
+                count_dist[l][n] = 1
+
             all_AA += denovo_list_remastered.repeats[TR].textD_standard_aa # string with all AA
-            # print(l,n)
-            l_n[l,n] += 1  
 
 ############ Create Dictionary with all AA appended ###############
 
-# Create dict which counts AAs
-AA_dict = (Counter(all_AA))
-# remove whitespace and gap
-AA_dict.pop(' ', None)
-AA_dict.pop('-', None)
+# # Create dict which counts AAs
+# AA_dict = (Counter(all_AA))
+# # remove whitespace and gap
+# AA_dict.pop(' ', None)
+# AA_dict.pop('-', None)
 
-# add missing AAs
-AAs = "ACDEFGHIKLMNPQRSTVWY"
-for AA in AAs:
-    if AA not in AA_dict:
-        AA_dict[AA] = 0
+# # add missing AAs
+# AAs = "ACDEFGHIKLMNPQRSTVWY"
+# for AA in AAs:
+#     if AA not in AA_dict:
+#         AA_dict[AA] = 0
 
-# sort by count
-sorted_AA_count = sorted(AA_dict.items(), key=lambda x: x[1], reverse=True)
-AA_names = list(zip(*sorted_AA_count))[0]
-AA_count = list(zip(*sorted_AA_count))[1]
-normed_AA_count = [float(i)/sum(AA_count) for i in AA_count]
+# # sort by count
+# sorted_AA_count = sorted(AA_dict.items(), key=lambda x: x[1], reverse=True)
+# AA_names = list(zip(*sorted_AA_count))[0]
+# AA_count = list(zip(*sorted_AA_count))[1]
+# normed_AA_count = [float(i)/sum(AA_count) for i in AA_count]
 
-# Plot
-plt.bar(AA_names, normed_AA_count, color='g')
-plt.title("Relative apearance of amino acids in tandem repeats in " + chr_name)
-plt.xlabel('Amino Acid')
-plt.ylabel('Relative appearance in TRs')
-plt.show()
-plt.close()
-
-############ Lenght/Unit Count Distribution ###############
-
-# Three subplots sharing both x/y axes
-# f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
-# plt.imshow(l_n[1,], cmap='hot')
-
-
-# print(l_n[1,])
-# # shiftig to have a better visualisation
-# l_n_roll = np.roll(l_n,1) # shift to the right 
-# l_n_roll2 = np.roll(l_n_roll,1,0) # shift down
-# print(l_n_roll2)
-
-# plt.xlabel("Unit Number")
-# plt.ylabel("Unit Lenght")
-# plt.xlim(1,max_length-1)
-# # plt.ylim(1,3)
-# plt.imshow(l_n_roll2, cmap='hot')
+# # Plot
+# plt.bar(AA_names, normed_AA_count, color='g')
+# # plt.title("Relative apearance of amino acids in tandem repeats in " + chr_name)
+# plt.xlabel('Amino Acid')
+# plt.ylabel('Relative appearance in TRs')
 # plt.show()
 # plt.close()
 
+############ Lenght/Unit Count Distribution ###############
+
+# get max count to define x-axis
+max_count = 0
+for n in range(1,4):
+    if max(count_dist[n]) > max_count:
+        max_count = max(count_dist[n])
+
+# add missing lengts
+for n in range(1,4):
+    for l in range(1,max_count+1):
+        if l not in count_dist[n]:
+            count_dist[n][l] = 0
+
+# sort values
+one_AA = list(sorted(count_dist[1].items()))
+two_AA = list(sorted(count_dist[2].items()))
+three_AA = list(sorted(count_dist[3].items()))
+
+length = list(zip(*one_AA))[0]
+one_AA_count = list(zip(*one_AA))[1]
+two_AA_count = list(zip(*two_AA))[1]
+three_AA_count = list(zip(*three_AA))[1]
+
+# Three subplots sharing both x/y axes
+f, (ax) = plt.subplots(3, sharey=True)
+
+ax[0].set(ylabel='Count', xlabel='Number of repetitive units with 1 Amino Acid.')
+ax[1].set(ylabel='Count', xlabel='Number of repetitive units with 2 Amino Acids.')
+ax[2].set(ylabel='Count', xlabel='Number of repetitive units with 3 Amino Acids.')
+
+ax[0].bar(length, one_AA_count, color='g')
+# ax[0].set_title('1 Amino Acid')
+ax[1].bar(length, two_AA_count, color='g')
+# ax[1].set_title('2 Amino Acids')
+ax[2].bar(length, three_AA_count, color='g')
+# ax[2].set_title('3 Amino Acids')
+f.subplots_adjust(hspace=0.5)
+
+plt.show()
+
 ########### Overview ###############
 
-print("\nOf {} proteins, {} contain a total of {} repeats.".format(number_proteins, number_TR_proteins, number_TRs))
-print("That is {} % of all proteins examined in this run.".format(round((number_TR_proteins / number_proteins * 100), 2)))
-print("\nFiltering criteria has been: \
-        \n --> pvalue < 0.05 \
-        \n --> divergence < 0.1 \
-        \n --> minimun repeat unit count: 2.5 \
-        \n --> maximum repeat unit length: 3\n")
+# print("\nOf {} proteins, {} contain a total of {} repeats.".format(number_proteins, number_TR_proteins, number_TRs))
+# print("That is {} % of all proteins examined in this run.".format(round((number_TR_proteins / number_proteins * 100), 2)))
+# print("\nFiltering criteria has been: \
+#         \n --> pvalue < 0.05 \
+#         \n --> divergence < 0.1 \
+#         \n --> minimun repeat unit count: 2.5 \
+#         \n --> maximum repeat unit length: 3\n")
